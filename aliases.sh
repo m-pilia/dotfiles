0532a92dbece68aedcd38e218347bcce9f23c735
Martino Pilia <martino.pilia@gmail.com>
2019-04-21 18:44:56 +0200

    Aliases for external monitor scaling and Python venv

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
-# shellcheck disable=2164
+# shellcheck disable=2164,2155
 
 alias ls='ls --color=auto'
 alias ll='ls -l'
@@ -17,4 +17,27 @@ alias pypi2pkgbuild='PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkg
 
 alias rgfzf='rg --no-line-number --no-heading . | fzf --delimiter=: --nth=2..'
 
-alias new_venv='python -m venv .venv && source .venv/bin/activate && pip install -r'
+# Create a Python virtual environment in a temporary directory
+function tmpvenv() {
+	if [ $# -lt 1 ] || [ ! -f "$1" ]; then
+		echo "usage: tmpvenv requirements.txt [venv ARGS]"
+		return
+	fi
+
+	local requirements=$1
+	shift
+	export TMPVENV_DIR=$(mktemp -d)
+
+	# shellcheck source=/dev/null
+	python -m venv "$@" "${TMPVENV_DIR}" && \
+	source "${TMPVENV_DIR}"/bin/activate && \
+	pip install -r "${requirements}"
+}
+
+alias external_monitor_off='xrandr --output DP-1 --off'
+
+function external_monitor_1080p() {
+	xrandr --output DP-1 --off
+	xrandr --output eDP-1 --scale "0.9999x0.9999" \
+	       --output DP-1 --auto --scale 2x2 --right-of eDP-1
+}
