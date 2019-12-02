a8fec366c55a1f3a55b598eb6e7282c8a6319cbc
Martino Pilia <martino.pilia@gmail.com>
2019-12-02 08:07:32 +0200

    Vi mode settings

---
+++ local/share/shell_config/spaceship_prompt_settings.zsh
@@ -0,0 +1,43 @@
+SPACESHIP_PROMPT_ORDER=(
+    time
+    user
+    host
+    dir
+    vi_mode
+    git
+    hg
+    package
+    node
+    ruby
+    elixir
+    xcode
+    swift
+    golang
+    php
+    rust
+    haskell
+    julia
+    docker
+    aws
+    venv
+    conda
+    pyenv
+    dotnet
+    ember
+    kubecontext
+    terraform
+    exec_time
+    line_sep
+    battery
+    jobs
+    exit_code
+    char
+    )
+
+SPACESHIP_VI_MODE_INSERT='i'
+SPACESHIP_VI_MODE_NORMAL='n'
+SPACESHIP_VI_MODE_PREFIX='('
+SPACESHIP_VI_MODE_SUFFIX=') '
+
+spaceship_vi_mode_enable
+
